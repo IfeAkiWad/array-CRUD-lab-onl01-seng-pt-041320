174bfdfe42af8dedf2e70fe60c99602eef2b60ca
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-17 05:36:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  birth_order = ["Ifeoluwa", "Oluwatoyin"]
-  birth_order << "Oluwatoyosi"
+  birth_order = []
+  birth_order << "Ifeoluwa"
     puts birth_order.inspect
   end
 end
